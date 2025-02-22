c4a386b58cafc702178bb06ba9d38c16b3754b1d
yashswi23 <yashswi2314@gmail.com>
2025-02-23 01:48:17 +0530

    Add collision Logic

--- ping.js
+++ ping.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let table = document.getElementById("ping-pong-table");
     let ball = document.getElementById("ball");
+    let paddle = document.getElementById("paddle");
     let ballX = 50;
     let ballY = 50;
 
@@ -15,6 +16,10 @@ document.addEventListener("DOMContentLoaded",()=>{
 
         ball.style.left =  `${ballX}px`;
         ball.style.top = `${ballY}px`;
+
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth && ballY> paddle.offsetTop && ballY+ ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight){
+            dx*=-1;
+        }
         if(ballX> table.offsetWidth - ball.offsetWidth || ballX<=0 ){
             dx*=-1;
         }
@@ -25,4 +30,21 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
     },1);
+
+    let paddleY=0;
+    let dpy =10;
+
+    document.addEventListener("keydown", (event) => {
+        event.preventDefault();
+        if (event.keyCode == 38 && paddleY>0) { // Up Arrow
+            paddleY+=(-1)*dpy;
+            console.log("Up"+ paddleY);
+           
+        } else if (event.keyCode == 40 && paddleY<table.offsetHeight-paddle.offsetHeight) { 
+            paddleY+= dpy// Down Arrow
+            console.log("Down" + paddleY);
+            
+        }
+        paddle.style.top = `${paddleY}px`
+    });
 })
